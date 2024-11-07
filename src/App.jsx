b31facf9c8988dc59f3c8f5f3bfc2722a7462a4f
Ferreira <98367479+alexfsxy@users.noreply.github.com>
2024-11-07 16:40:06 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import './Style.css'
+import PageSobre from './components/PageSobre.jsx'
+import Login from './components/Login.jsx'
+
+function App() {
+
+  return (
+
+   <>
+< PageSobre />    
+ </>
+  )
+}
+export default App
